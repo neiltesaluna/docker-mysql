135d373ed8feca116c753a32b807662fcfd84254
Neil Tesaluna <neil.tesaluna@gmail.com>
2021-04-21 12:14:25 +1000

    added response check

--- project/app.py
+++ project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 import pymysql
@@ -22,6 +22,16 @@ class Greetings(db.Model):
 
 db.create_all()
 
+
+def receive_response():
+    try:
+        data = request.get_json(force=True)
+    except:
+        data = request.args
+    return data
+
+
+
 @app.route('/', methods=['GET'])
 def home():
     
@@ -31,7 +41,31 @@ def home():
     except:
         greetings = [{'sayhi':'I can\'t understand you, seems like the database is empty'}]
 
+
     return render_template('index.html', greetings = greetings)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+
+    data = receive_response()
+    greeting = data.get('greeting')
+
+    new_greeting = Greetings(sayhi=greeting)
+    db.session.add(new_greeting)
+    db.session.commit()
+
+    return f'The database has been updated with {greeting}\n'
+
+@app.route('/methods', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def methods():
+    
+    data = receive_response()
+    data = data.get('greeting')
+
+
+    return f'You\'ve sent a {request.method} request\nwith some data:\n{data}\n'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
