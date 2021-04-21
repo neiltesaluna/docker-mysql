fbb0ed94f471924e3b1e94f30c479a68663dd98b
Neil Tesaluna <neil.tesaluna@gmail.com>
2021-04-21 15:27:27 +1000

    added new api route, fixed up bugs

--- project/app.py
+++ project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 import pymysql
@@ -20,14 +20,29 @@ class Greetings(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sayhi = db.Column(db.String(20), nullable=False)
 
+# creating database tables
 db.create_all()
 
 
-def receive_response():
+def receive_response(param):
+
+    # check if json data
     try:
         data = request.get_json(force=True)
+        data = data.get(param)
+
     except:
-        data = request.args
+        # check if form data
+        if request.form.get(param):
+            data = request.form.get(param)
+
+        #check if query string data
+        elif request.args.get(param):
+            data = request.args.get(param)
+
+        else:
+            data = 'data not found!'
+
     return data
 
 
@@ -48,23 +63,36 @@ def home():
 @app.route('/add', methods=['POST'])
 def add():
 
-    data = receive_response()
-    greeting = data.get('greeting')
+    data = receive_response('greeting')
 
-    new_greeting = Greetings(sayhi=greeting)
-    db.session.add(new_greeting)
+    greeting = Greetings(sayhi=data)
+    db.session.add(greeting)
     db.session.commit()
 
-    return f'The database has been updated with {greeting}\n'
+    return f'The database has been updated with {data}\n'
 
 @app.route('/methods', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def methods():
     
-    data = receive_response()
-    data = data.get('greeting')
-
+    data = receive_response('greeting')
 
     return f'You\'ve sent a {request.method} request\nwith some data:\n{data}\n'
+
+
+
+@app.route('/api', methods=['GET'])
+def api():
+    try:
+        greetings = [greetings.__dict__ for greetings in Greetings.query.all()]
+        for item in greetings:
+            item.pop('_sa_instance_state')
+        
+    except:
+        greetings = [{'sayhi':'I can\'t understand you, seems like the database is empty'}]
+
+    json = jsonify(greetings)
+    return json
+
 
 
 if __name__ == '__main__':
